470e13acafd17cb80194eaa3085c77883ea192fb
datawolf <wanglong@cse.buaa.edu.cn>
2013-08-07 22:50:06 +0800

    ansi c

--- audition/struct.c
+++ audition/struct.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-
+//结构体类型的定义
 typedef struct  human_being {
     char    name[10];
     int     age;
     float   salary;
 } human_being_t;
+
+//ANSI C允许结构体之间相互赋值
 int main()
 {
     human_being_t person1, person2;
+    
+    person1.age = 10;
+    person2 = person1;
 
+    printf("person2.age = %d\n", person2.age);
     return 0;
 }
 
