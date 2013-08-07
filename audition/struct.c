962d45bb873a260c928188c0f9f4b470cecdd89c
datawolf <wanglong@cse.buaa.edu.cn>
2013-08-07 22:45:53 +0800

    struct test

---
+++ audition/struct.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct  human_being {
+    char    name[10];
+    int     age;
+    float   salary;
+} human_being_t;
+int main()
+{
+    human_being_t person1, person2;
+
+    return 0;
+}
+
