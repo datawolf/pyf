91e80829026ed9c96de15f4bf7dd041d419833ea
datawolf <wanglong@cse.buaa.edu.cn>
2013-08-23 17:23:16 +0800

    add srand function

--- sort/permute.c
+++ sort/permute.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <time.h>
 #include "type.h"
 
 void permute(element list[], int n)
@@ -8,6 +9,7 @@ void permute(element list[], int n)
 
    for (i = n-1; i >= 1; i--)
    {
+        srand((int)time(NULL));
         j = rand() % (n-1) + 1;
         SWAP(list[i], list[j], temp)
    }
