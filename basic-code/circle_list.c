6a260922d82c2ad2ee78bbbd122b10a689407a80
datawolf <wanglong@cse.buaa.edu.cn>
2013-10-30 19:01:49 +0800

    add circle_list

---
+++ basic-code/circle_list.c
@@ -0,0 +1,37 @@
+#include "circle_list.h"
+p_circle_list_t     circle_list_create(void)
+{
+
+}
+int     circle_list_insert(p_circle_list_t list, int num, void* pitem, size_t n)
+{
+
+}
+void    circle_list_delete(p_circle_list_t list, int num)
+{
+
+}
+void    circle_list_update(p_circle_list_t list, int num, void* pitem, size_t n)
+{
+
+}
+void    circle_list_trav(p_circle_list_t list, void(*func)(void*))
+{
+
+}
+void    circle_list_destroy(p_circle_list_t list)
+{
+
+}
+void*   circle_list_visit(p_circle_list_t list, int num)
+{
+
+}
+int     circle_list_search(p_circle_list_t list, void* pitem, int (*comp)(void*, void*))
+{
+
+}
+void    circle_list_rotate(p_circle_list_t list, int num)
+{
+
+}
---
+++ basic-code/circle_list.h
@@ -0,0 +1,41 @@
+#ifndef     _CIRCLE_LIST_H
+#define     _CIRCLE_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+typedef     struct double_list_node
+{
+    struct double_list_node *next;
+    struct double_list_node *prev;
+
+    void*   p_data;
+} double_list_node_t;
+
+typedef     double_list_node_t*     p_double_list_node_t;
+
+typedef     struct circle_list
+{
+    p_double_list_node_t    head;
+    p_double_list_node_t    tail;
+    int size;
+} circle_list_t;
+
+typedef     circle_list_t*      p_circle_list_t;
+
+extern      int     errno;
+
+p_circle_list_t     circle_list_create(void);
+int     circle_list_insert(p_circle_list_t, int, void*, size_t);
+void    circle_list_delete(p_circle_list_t, int);
+void    circle_list_update(p_circle_list_t, int, void*, size_t);
+void    circle_list_trav(p_circle_list_t, void(*)(void*));
+void    circle_list_destroy(p_circle_list_t);
+void*   circle_list_visit(p_circle_list_t, int);
+int     circle_list_search(p_circle_list_t, void*, int (*)(void*, void*));
+void    circle_list_rotate(p_circle_list_t, int);
+
+#endif      /*_CIRCLE_LIST_H */
